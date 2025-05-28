6d67a777b95874c599bbdf4b3a927dbbd9af7fd9
Matheus Oliveira <matmig17@gmail.com>
2025-05-28 11:12:20 -0300

    adicionando landing page para entendimento do projeto back api

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import express, { Express } from "express";
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from "../generated/prisma";
 import userRouter from "./routes/userRoutes";
 import postRouter from "./routes/postRoutes";
-// import postRouter from "./routes/postRoutes";
+import path from "path";
 
 const app: Express = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(userRouter);
 app.use(postRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
